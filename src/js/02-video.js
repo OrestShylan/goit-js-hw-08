3a17337e450d14bac51a0bca58f48491b9c33fa5
OrestShylan <orestshylan1997@gmail.com>
2023-01-29 18:02:37 +0200

    fixed file vide.js, remove function setTime

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,21 @@
-import player from '@vimeo/player';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'videoplayer - current - time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-function saveTime(data) {
+function setTime(data) {
   const time = data.seconds;
-  localStorage.saveTime(STORAGE_KEY, time);
+  localStorage.setItem(STORAGE_KEY, time);
 }
 
 function updateTime() {
-  const currentTime = localStorage.getitem(STORAGE_KEY);
+  const currentTime = localStorage.getItem(STORAGE_KEY);
 
   if (currentTime) {
     player.setCurrentTime(currentTime);
   }
 }
 updateTime();
-player.on('timeupdate', throttle(aveTime));
+player.on('timeupdate', throttle(setTime, 1000));
