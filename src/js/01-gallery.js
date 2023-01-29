afec79bb8a310b1ebbb52f0c4fdc16d4d8e6c842
OrestShylan <orestshylan1997@gmail.com>
2023-01-29 16:49:06 +0200

    add vinemo

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const createGalleryMarkup = creatsImgGellery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', createGalleryMarkup);
+
+function creatsImgGellery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    `;
+    })
+    .join(' ');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+});
